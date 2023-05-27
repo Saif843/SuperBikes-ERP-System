f092dbb8575f58e07041a229cb9659423b8a3356
Saif Ali <81072571+Saif843@users.noreply.github.com>
2023-05-27 11:03:47 +0500

    Update production_controller.ex
    Added multi-line comments by keep in mind of refinement issues

--- erp_backend/lib/erp_web/controllers/production_controller.ex
+++ erp_backend/lib/erp_web/controllers/production_controller.ex
@@ -4,6 +4,22 @@ A module that acts as the controller for production of products.
 """
   use ErpWeb, :controller
 
+
+## Examples
+      iex> get_production_info(conn, %{})
+      {:ok, %{productionData: ...}}
+      iex> get_production_info(conn, %{})
+      {:error, :error}
+  """
+  def get_production_info(conn, _params) do
+    case Erp.Production.production_status() do
+      {:ok, data} -> json(conn, %{productionData: data})
+      {:error, _} -> {:error, :error}
+    end
+  end
+end
+
+
   @doc """
   Show the production information for the plant.
   """
